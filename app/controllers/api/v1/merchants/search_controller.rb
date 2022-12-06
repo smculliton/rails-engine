3d30c6ed68eb80ecbf3fc765b8fdfb5684b51f09
Sean Culliton <smculliton@gmail.com>
2022-12-06 15:53:48 -0700

    Finish merchant search

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Merchants::SearchController < ApplicationController
-  def show
-    
+  # is there a better way to handle this sad path?
+  def show 
+    merchant = Merchant.search(params[:name]) || Merchant.new
+    # return render json: {data: {} } if merchant.nil?
+
+    render json: MerchantSerializer.new(merchant)
   end
 end
--- spec/requests/merchants_find_spec.rb
+++ spec/requests/merchants_find_spec.rb
@@ -1,11 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'the Merchant Search' do 
+  let!(:merchants) { create_list(:merchant, 10) }
+
   describe 'GET /api/v1/merchants/find' do 
     it 'searches merchants by name' do 
-      get '/api/v1/merchants/find', params: { name: 'mart' }
-      
+      keyword = merchants.first.name
+      get '/api/v1/merchants/find', params: { name: keyword }
+
+      json = JSON.parse(response.body, symbolize_names: true)
       expect(response).to have_http_status(200)
+
+      expect(json[:data]).to be_a Hash
+
+      expect(json[:data]).to have_key(:attributes)
+      expect(json[:data][:attributes]).to be_a Hash
+
+      expect(json[:data]).to have_key(:id)
+      expect(json[:data]).to have_key(:type)
+    end
+
+    describe 'sad paths' do 
+      it 'returns empty hash if no matches' do 
+        get '/api/v1/merchants/find', params: { name: '###' }
+
+        json = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to have_http_status(200)
+
+        expect(json[:data][:id]).to eq(nil)
+      end
     end
   end
 end
